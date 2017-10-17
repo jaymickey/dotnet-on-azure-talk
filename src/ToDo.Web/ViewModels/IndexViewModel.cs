63f0410e185e66ab880ef17f7c82527aceacf1bb
Josh Michielsen <jmichiel@myune.edu.au>
2017-10-17 22:33:26 +0800

    Initial code commit

---
+++ src/ToDo.Web/ViewModels/IndexViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using ToDo.Web.Models;
+
+namespace ToDo.Web.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<TaskItem> AllTasks { get; set; }
+        public NewTaskViewModel NewTask { get; set; }
+    }
+}
